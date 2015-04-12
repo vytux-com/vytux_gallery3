c4937533aaba9bab191c7d5b057326f91269d8c0
Vytautas Krivickas <vytautas.krivickas@gmail.com>
2015-04-12 19:38:52 +1000

    Tidy up language code in edit

--- module.php
+++ module.php
@@ -400,16 +400,15 @@ class VytuxGallery3Module extends webtrees\AbstractModule implements webtrees\Mo
 					<label class="control-label col-sm-3" for="lang_*">
 						<?php echo webtrees\I18N::translate('Show this album for which languages?'); ?>
 					</label>
-					<div class="row col-sm-9">
+					<div class="col-sm-9">
 						<?php 
 							$accepted_languages=explode(',', $this->getBlockSetting($block_id, 'languages'));
 							foreach (webtrees\I18N::installedLocales() as $locale) {
-								$code = $locale->languageTag();
-								$name = $locale->endonym();
-								$checked = in_array($code, $accepted_languages) ? 'checked' : ''; 
 						?>
-								<div class="col-sm-3">
-									<label class="checkbox-inline "><input type="checkbox" name="lang_<?php echo $code; ?>" <?php echo $checked; ?> ><?php echo $name; ?></label>
+								<div class="checkbox">
+									<label title="<?php echo $locale->languageTag(); ?>">
+										<input type="checkbox" name="lang_<?php echo $locale->languageTag(); ?>" <?php echo in_array($locale->languageTag(), $accepted_languages) ? 'checked' : ''; ?> ><?php echo $locale->endonym(); ?>
+									</label>
 								</div>
 						<?php 
 							}
